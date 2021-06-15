c3b784febc5eba2631d01d5db291bc4727896024
Thalia Stemitsiotis <thalia.stemitsiotis@gmail.com>
2021-06-15 17:36:18 +0100

    frontend in progress

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import requests
+import json
 import pandas as pd
 import numpy as np
+from bs4 import BeautifulSoup
+
+
 
 '''
 # The Fantasy Football Predictor ⚽️🏆
@@ -14,17 +18,19 @@ st.markdown('''
 
 st.markdown('___')
 
+#with st.form(key='my_form'):
+
 st.subheader('Select 3 forwards *e.g. Harry Kane*')
 options_forward = ('', 'Kevin de Bruyne', 'marc zuck', 'john brain', 'John Stones')
 
-fwd_1 = st.multiselect('', options_forward)
+fwd_1 = st.multiselect('', options_forward, key="1")
 #fwd_2 = st.text_input('Forward 2')
 #fwd_3 = st.text_input('Forward 3')
 
 st.subheader('Select 5 midfielders *e.g. Kevin de Bruyne*')
-options_midfielders = ('', 'Kevin de Bruyne', 'marc zuck', 'john brain')
+options_midfielders = ('', 'Kevin de Bruyne', 'marc zuck', 'john brain', 'John Stones', 'Hugo Lloris')
 
-mid_1 = st.multiselect('', options_midfielders)
+mid_1 = st.multiselect('', options_midfielders, key="2")
 #mid_2 = st.text_input('Midfielder 2')
 #mid_3 = st.text_input('Midfielder 3')
 #mid_4 = st.text_input('Midfielder 4')
@@ -33,7 +39,7 @@ mid_1 = st.multiselect('', options_midfielders)
 st.subheader('Select 5 defenders *e.g. John Stones*')
 options_defenders = ('', 'Kevin de Bruyne', 'marc zuck', 'john brain', 'John Stones')
 
-def_1 = st.multiselect('', options_defenders)
+def_1 = st.multiselect('', options_defenders, key="3")
 #def_2 = st.text_input('Defender 2')
 #def_3 = st.text_input('Defender 3')
 #def_4 = st.text_input('Defender 4')
@@ -42,7 +48,7 @@ def_1 = st.multiselect('', options_defenders)
 st.subheader('Select 2 goalkeepers *e.g. Hugo Lloris*')
 options_goalkeepers = ('', 'Kevin de Bruyne', 'marc zuck', 'john brain', 'John Stones', 'Hugo Lloris')
 
-gk_1= st.multiselect('', options_goalkeepers)
+gk_1= st.multiselect('', options_goalkeepers, key="4")
 #gk_2= st.text_input('Goalkeeper 2')
 
 st.markdown('___')
@@ -63,27 +69,47 @@ st.markdown('''
 
 # url = ''
 params = pd.DataFrame({
-          'names': (fwd_1, fwd_1, fwd_1, 
-                    mid_1, mid_1, mid_1, mid_1, mid_1, 
-                    def_1, def_1, def_1, def_1, def_1, 
-                    gk_1, gk_1),
-          'positions': ('fwd', 'fwd', 'fwd', 
-                        'mid', 'mid', 'mid', 'mid', 'mid', 
-                        'def', 'def', 'def', 'def', 'def', 
-                        'gk', 'gk')
-        })
+    'names': (fwd_1, fwd_1, fwd_1, 
+                mid_1, mid_1, mid_1, mid_1, mid_1, 
+                def_1, def_1, def_1, def_1, def_1, 
+                gk_1, gk_1),
+    'positions': ('fwd', 'fwd', 'fwd', 
+                    'mid', 'mid', 'mid', 'mid', 'mid', 
+                    'def', 'def', 'def', 'def', 'def', 
+                    'gk', 'gk')
+    })
 
 #          'names': (fwd_1, fwd_2, fwd_3, 
 #                    mid_1, mid_2, mid_3, mid_4, mid_5, 
 #                    def_1, def_2, def_3, def_4, def_5, 
 #                    gk_1, gk_2)
 
-if st.button('Start the Predictions'):
+# Players pictures
+
+def get_picture(player_name):
+    url1 = f"https://sofifa.com/players?keyword={player_name}"
+    doc = requests.get(url1)
+    soup = BeautifulSoup(doc.content, 'html.parser')
+    try:
+      table = soup.select('.column.col-auto')[0]
+      #img_url=table.img['data-srcset'].split(',')[-1][1:-3]
+      img_url = table.img['data-src'][0:-9] + '21_180.png'
+      image = img_url
+    except Exception as e:
+      print('Not found ... ')
+      image = 'not found'
+    return image
+
+     
+# Button to predict
+
+if 	st.button('Start the Predictions'): #st.form_submit_button(label='Submit'):
     import time
-    
+
     'AI computed predictions in progress...'
-    
+
     # response = requests.get(url, params).json()
+    response = json.load(open("test.json"))
 
     # Add a placeholder
     latest_iteration = st.empty()
@@ -95,74 +121,102 @@ if st.button('Start the Predictions'):
         bar.progress(i + 1)
         time.sleep(0.1)
         
-    'Here are our predictions:'
+    #st.markdown('''
+    ## Here are our predictions :
+    #''')
     
-    # top_transfer_1 = response['']
-    # top_transfer_2 = response['']    
-    # top_transfer_3 = response['']  
+    #import ipdb; ipdb.set_trace()
+    # Transfer players
+    top_transfer_players = list(response['best_transfers']['leaving_player'].values())
+    top_transfer_subs = list(response['best_transfers']['incoming_player'].values())
     
-    # sugg_player_1 = response['']
-    # sugg_player_1_p = response['']
+    top_transfer_1 = top_transfer_players[0].title()
+    top_transfer_2 = top_transfer_players[1].title()   
+    top_transfer_3 = top_transfer_players[2].title() 
+
+    top_sub_1 = top_transfer_subs[0].title()
+    top_sub_2 = top_transfer_subs[1].title()   
+    top_sub_3 = top_transfer_subs[2].title()
     
-    # sugg_player_2 = response['']
-    # sugg_player_2_p = response['']
+    # Suggested players
+
+    sugg_players = list(response['best_11']['name'].values())
+    sugg_players_position = list(response['best_11']['position'].values())
+
+    sugg_player_1 = sugg_players[0].title()
+    sugg_player_1_p = sugg_players_position[0]
+
+    sugg_player_2 = sugg_players[1].title()
+    sugg_player_2_p = sugg_players_position[1]
         
-    # sugg_player_3 = response['']
-    # sugg_player_3_p = response['']
+    sugg_player_3 = sugg_players[2].title()
+    sugg_player_3_p = sugg_players_position[2]
 
-    # sugg_player_4 = response['']
-    # sugg_player_4_p = response['']
-
-    # sugg_player_5 = response['']
-    # sugg_player_5_p = response['']
-
-    # sugg_player_6 = response['']
-    # sugg_player_6_p = response['']
-
-    # sugg_player_7 = response['']
-    # sugg_player_7_p = response['']
-
-    # sugg_player_8 = response['']
-    # sugg_player_8_p = response['']
-
-    # sugg_player_9 = response['']
-    # sugg_player_9_p = response['']
-
-    # sugg_player_10 = response['']
-    # sugg_player_10_p = response['']
-
-    # sugg_player_11 = response['']
-    # sugg_player_11_p = response['']
+    sugg_player_4 = sugg_players[3].title()
+    sugg_player_4_p = sugg_players_position[3]
     
-    # sugg_captain = response['']
-    # sugg_vice_captain = response['']
-    
-    # bench_player_1 = response['']
-    # bench_player_1_p = response['']
-    # bench_player_2 = response['']
-    # bench_player_2_p = response['']
-    # bench_player_3 = response['']
-    # bench_player_3_p = response['']
+    sugg_player_5 = sugg_players[4].title()
+    sugg_player_5_p = sugg_players_position[4]
 
+    sugg_player_6 = sugg_players[5].title()
+    sugg_player_6_p = sugg_players_position[5]
 
-    ## st.success(f'Prediction: {x}')
+    sugg_player_7 = sugg_players[6].title()
+    sugg_player_7_p = sugg_players_position[6]
+
+    sugg_player_8 = sugg_players[7].title()
+    sugg_player_8_p = sugg_players_position[7]
     
-    #st.write(f'The players to transfer for this Gameweek: 
-    #         {top_transfer_1}, {top_transfer_2}, {top_transfer_3}')
+    sugg_player_9 = sugg_players[8].title()
+    sugg_player_9_p = sugg_players_position[8]
+
+    sugg_player_10 = sugg_players[9].title()
+    sugg_player_10_p = sugg_players_position[9]
     
-    #st.write(f'Suggestion of the 11 starting players: 
-    #         {sugg_player_1} ({sugg_player_1_p}), {sugg_player_2} ({sugg_player_2_p}), 
-    #         {sugg_player_3} ({sugg_player_3_p}), {sugg_player_4} ({sugg_player_4_p}),
-    #         {sugg_player_5} ({sugg_player_5_p}), {sugg_player_6} ({sugg_player_6_p}),
-    #         {sugg_player_7} ({sugg_player_7_p}), {sugg_player_8} ({sugg_player_8_p}), 
-    #         {sugg_player_9} ({sugg_player_9_p}), {sugg_player_10} ({sugg_player_10_p}), 
-    #         {sugg_player_11} ({sugg_player_11_p})')
+    sugg_player_11 = sugg_players[10].title()
+    sugg_player_11_p = sugg_players_position[10]
     
-    #st.write(f'Suggestion of a Captain and Vice-Captain:
-    #         {sugg_captain} & {sugg_vice_captain}')
+    # Captain and Vice Captain 
+
+    sugg_captain = response['captain'].title()
+    sugg_vice_captain = response['vice_captain'].title()
+
+    # Bench players
     
-    #st.write(f'Suggestion of the 4 bench players:
-    #         {bench_player_1} ({bench_player_1_p}), 
-    #         {bench_player_2} ({bench_player_2_p}), 
-    #         {bench_player_3} ({bench_player_3_p})')
+    bench_players = list(response['subs_4']['name'].values())
+    bench_players_position = list(response['subs_4']['position'].values())
+
+    bench_player_1 = bench_players[0].title()
+    bench_player_1_p = bench_players_position[0]
     
+    bench_player_2 = bench_players[1].title()
+    bench_player_2_p = bench_players_position[1]
+    
+    bench_player_3 = bench_players[2].title()
+    bench_player_3_p = bench_players_position[2]
+    
+    bench_player_4 = bench_players[3].title()
+    bench_player_4_p = bench_players_position[3]
+
+    st.markdown('___')
+
+    st.write(f"""**Starting 11 :**   
+            *Forwards*: {sugg_player_1} ({sugg_player_1_p}), {sugg_player_2} ({sugg_player_2_p}) and {sugg_player_3} ({sugg_player_3_p})                                         
+            *Midfielders*: {sugg_player_4} ({sugg_player_4_p}), {sugg_player_5} ({sugg_player_5_p}), {sugg_player_6} ({sugg_player_6_p}) and {sugg_player_7} ({sugg_player_7_p})  
+            *Defenders*: {sugg_player_8} ({sugg_player_8_p}), {sugg_player_9} ({sugg_player_9_p}) and {sugg_player_10} ({sugg_player_10_p})                                     
+            *Goalkeeper*: {sugg_player_11} ({sugg_player_11_p})""")
+    
+    st.write(f"""**Bench players :**
+             {bench_player_1} ({bench_player_1_p}), 
+             {bench_player_2} ({bench_player_2_p}), 
+             {bench_player_3} ({bench_player_3_p})""")
+
+    st.write(f"""**Captain :** {sugg_captain}""")
+    
+    st.write(f"""**Vice-Captain :** {sugg_vice_captain}""")
+    
+    st.write(f"""**Transfers for this Gameweek :**                 
+             {top_transfer_1} ➣ {top_sub_1}         
+             {top_transfer_2} ➣ {top_sub_2}         
+             {top_transfer_3} ➣ {top_sub_3}
+             """)
